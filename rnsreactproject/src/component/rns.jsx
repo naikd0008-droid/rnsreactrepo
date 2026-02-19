149d149571e384e5fbfdc8526b05075754ffe5af
naikd0008-droid <naikd0008@gmail.com>
2026-02-19 10:21:31 +0530

    first commit

--- rnsreactproject/src/component/rns.jsx
+++ rnsreactproject/src/component/rns.jsx
@@ -5,15 +5,55 @@ function Rns() {
   return (
     <>
     <div className='main'>
-        <div className='top'></div>
+        <div className='top'>
+          <div className='topleft'></div>
+          <div className='topright'></div>
+        </div>
         <div className='middle'>
-            <div className='t1'></div>
-            <div className='t2'></div>
+            <div className='t1'>
+              My Orders
+            </div>
+            <div className='t2'>
+              <div className='tt1'></div>
+              <div className='tt2'></div>
+              <div className='tt3'></div>
+              <div className='tt4'></div>
+            </div>
         </div>
         <div className='bottom'>
-            <div className='b1'></div>
-            <div className='b2'></div>
-            <div className='b3'></div>
+            <div className='b1'>
+              <div className='bb1'></div>
+              <div className='bb2'>
+                <div className='a1'></div>
+                <div className='a2'></div>
+                <div className='a3'></div>
+                <div className='a4'></div>
+              </div>
+              <div className='bb3'></div>
+            </div>
+            <div className='b2'>
+              <div className='bbb1'></div>
+              <div className='bbb2'>
+                <div className='c1'></div>
+                <div className='c2'></div>
+                <div className='c3'></div>
+                <div className='c4'></div>
+              </div>
+              <div className='bbb3'></div>
+            </div>
+            <div className='b3'>
+              <div className='bbbb1'></div>
+              <div className='bbbb2'>
+                <div className='d1'></div>
+                <div className='d2'></div>
+                <div className='d3'></div>
+                <div className='d4'></div>
+              </div>
+              <div className='bbbb3'>
+                <button className='but1'></button>
+                <button className='but2'></button>
+              </div>
+            </div>
         </div>
     </div>
   
