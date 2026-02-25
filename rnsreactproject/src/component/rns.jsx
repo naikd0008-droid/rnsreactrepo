cd54c5fa51089b3bcf367d66b62e654ec81194cc
naikd0008-droid <naikd0008@gmail.com>
2026-02-25 10:30:44 +0530

    update

--- rnsreactproject/src/component/rns.jsx
+++ rnsreactproject/src/component/rns.jsx
@@ -7,9 +7,6 @@ import { LuEye } from "react-icons/lu";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
-
-
-
 function Rns() {
 
   return (
@@ -20,12 +17,12 @@ function Rns() {
               <b>My Orders</b>
             </div>
             <div className='t2'>
-              <div className='tt1'>
+              <div className='tt1'><div className='drn1'>
                 <button className='butt1'>All (3)</button>
                 <button className='butt2'>Pending (1)</button>
                 <button className='butt3'>In Progress (1)</button>
                 <button className='butt4'>Completed (1)</button>
-              </div>
+              </div></div>
               <div className='tt2'></div>
             </div>
         </div>
@@ -36,9 +33,10 @@ function Rns() {
               </div>
               <div className='bb2'>
                 <div className='a1'><b>Rajesh Kumar</b></div>
-                <div className='a2'>Birthday for Sneha Sharma</div>
-                <div className='a3'> Happy Birthday to my amazing sister!Wishing you all the success.</div>
-                <div className='a4'>₹5,000</div>
+                <div className='a2'> <div className='a21'>Birthday for Sneha Sharma  </div>
+                <div className='a22'> Happy Birthday to my amazing sister! Wishing you all the success.</div>
+                </div>
+                <div className='a4'><b>₹5,000</b></div>
               </div>
               <div className='bb3'>
                 <button className='button1'><FaRegClock/> Pending</button>
@@ -50,9 +48,11 @@ function Rns() {
               </div>
               <div className='bbb2'>
                 <div className='c1'><b>Meera Patel</b></div>
-                <div className='c2'>Anniversary for Priya and Raj </div>
-                <div className='c3'>Happy anniversary to the most beautiful couple!</div>
-                <div className='c4'> ₹3,000</div>
+                <div className='c2'>
+                <div className='c21'>Anniversary for Priya and Raj </div>
+                <div className='c22'>Happy anniversary to the most beautiful couple! </div>
+                </div>
+                <div className='c4'><b> ₹3,000</b></div>
               </div>
               <div className='bbb3'>
                 <button className='button2'><FaRegClock/> In Progress</button>
@@ -64,9 +64,10 @@ function Rns() {
               </div>
               <div className='bbbb2'>
                 <div className='d1'><b>Tanvi Mehta</b></div>
-                <div className='d2'>Best Wishes for Amit Kumar</div>
-                <div className='d3'>Good luck with your new venture! You will do amazing!</div>
-                <div className='d4'>₹3,500</div>
+                <div className='d2'><div className='d21'>Best Wishes for Amit Kumar</div>
+                <div className='d22'> Good luck with your new venture! You will do amazing!</div>
+               </div>
+                <div className='d4'><b>₹3,500</b></div>
               </div>
               <div className='bbbb3'>
                 <div className='darshh'>
@@ -80,9 +81,6 @@ function Rns() {
               </div>
             </div>
         </div>
-  
-    
-  
   
     </>
   )
